5de7338446210f9748e0dd58377a27102aa79388
Matt Pahuta <mhpahuta@gmail.com>
2024-01-28 15:20:19 -0800

    Add mobile nav menu and functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Scripts connected')
+console.log('Scripts connected');
+
+const navBtn = document.querySelector('#mobile-nav-toggle');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelector('.nav-links');
+
+navBtn.addEventListener('click', () => {
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  !isExpanded && nav.classList.add('active');
+
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  
+})
