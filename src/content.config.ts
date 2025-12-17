b0e2ed6a705e2d89df3f3597c4eec72fc9256dbc
Matt Pahuta <mhpahuta@gmail.com>
2025-12-17 15:42:50 -0800

    Add Astro content collection config

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
+
+const project = defineCollection({
+  loader: file('src/data/projects.json'),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    techStack: z.array(z.object({
+      name: z.string(),
+      icon: z.string()
+    })),
+    imagePath: z.string(),
+    imageAltText: z.string(),
+    gitHubUrl: z.string().url(),
+    liveSiteUrl: z.string().url()
+  })
+});
+
+export const collections = { project }
